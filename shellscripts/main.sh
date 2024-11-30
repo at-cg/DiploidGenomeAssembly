f3825b8a8236c6772f7b20203b20958b701cc9e5
daanishmahajan <daanishmahajan27@gmail.com>
2024-12-01 02:21:10 +0530

    v1

--- shellscripts/main.sh
+++ shellscripts/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N dipCall
+#PBS -N RepeatStats
 #PBS -l nodes=1:ppn=48
 #PBS -q regular
 #PBS -l walltime=24:00:00
@@ -8,10 +8,25 @@
  
 source ~/.bashrc
 
-EXE=/home/daanish/projects/repeatstatistics/code/mat_triple
+LOG=/home/daanish/projects/DiploidGenomeAssembly/src/log.txt
 
-cd /home/daanish/projects/repeatstatistics/code
-rm op.log op.err
-/usr/bin/time -v $EXE
+cd /home/daanish/projects/DiploidGenomeAssembly/src
+make -f makefile_intradouble clean
+make -f makefile_intradouble
 
+echo "Starting test" >> $LOG
+
+for i in {1..100}; do
+    EXE=/home/daanish/projects/DiploidGenomeAssembly/src/test.py
+    python3 $EXE
+    INPUT=/home/daanish/projects/DiploidGenomeAssembly/src/test.fasta
+    OUTPUT=/home/daanish/projects/DiploidGenomeAssembly/src/test_intradoublerepeat.txt
+
+    export LD_LIBRARY_PATH=/home/daanish/projects/DiploidGenomeAssembly/libdivsufsort/build/lib:$LD_LIBRARY_PATH
+    ./intradouble $INPUT $OUTPUT
+
+    echo "Iteration $i finished" >> $LOG
+done
+
+echo "Ending test" >> $LOG
 
